07815ce06da73dffe6b79965374d5c249aabb22c
Juha Syrjälä <Juha.Syrjala@iki.fi>
2008-09-27 14:25:04 +0300

    added colors for robots and border around arena

--- pygame_visualizer.py
+++ pygame_visualizer.py
@@ -14,9 +14,11 @@ from controllers.sniper import *
 class PyGameVisualizer(Visualizer):
     def __init__(self):
         self.size = self.width, self.height = 500, 500
+        self.screen_size = self.screen_width, self.screen_height = 600, 600
+        self.offset = 50
         pygame.init()
  
-        self.screen = pygame.display.set_mode(self.size)
+        self.screen = pygame.display.set_mode(self.screen_size)
         
     def visualize(self, sim):
         self.sim = sim
@@ -24,8 +26,6 @@ class PyGameVisualizer(Visualizer):
         self.draw()
     
     def draw(self):
-    
-        
         self.draw_background()
         self.draw_robots()
         self.draw_shells()
@@ -35,11 +35,15 @@ class PyGameVisualizer(Visualizer):
     def draw_background(self):
         black = 0, 0, 0
         self.screen.fill(black)
-    
+
+        rect = [self.offset, self.offset, self.screen_width - 2*self.offset, self.screen_height - 2*self.offset]
+        pygame.draw.rect(self.screen, [255,255,255], rect, 1)
+        
     def draw_robots(self):
-        color = [100,200,100]
+        
         for player in self.sim.players:
             robot = player.robot
+            color = player.color
             self.draw_circle(self.convert_location(robot.location), self.convert_width(robot.radius), color )
             
     def draw_shells(self):
@@ -56,9 +60,7 @@ class PyGameVisualizer(Visualizer):
                 sys.exit()
 
     def convert_location(self, location):
-        
-        
-        return [  self.convert_width(location[0]), self.convert_height(location[1]) ]
+        return [  self.convert_width(location[0]) + self.offset, self.convert_height(location[1]) + self.offset ]
         
     def convert_width(self, width):
         x = float(width) * self.width / ARENA_MAX_WIDTH
@@ -69,17 +71,18 @@ class PyGameVisualizer(Visualizer):
         return int(y)
 
 if __name__ == '__main__':
-    def create_player(name, controller):
+    def create_player(name, controller, color):
         robot = Robot([ randint(1000, 9000), randint(1000, 9000) ], 'R2')
         robot.direction = randint(0,359)
         player = Player(robot, controller())
+        player.color = color
         return player
         
     vis = PyGameVisualizer()
     
-    pl1 = create_player("cylon", Cylon)
-    pl2 = create_player("tracer", Tracker)
-    pl3 = create_player("sniper", Sniper)
+    pl1 = create_player("cylon", Cylon, [255,255,255] )
+    pl2 = create_player("tracer", Tracker, [40, 255, 255] )
+    pl3 = create_player("sniper", Sniper, [255, 0, 0] )
     
     sim = Simulator([pl1, pl2, pl3], vis)
     
